12118732fbbae6266a53974ad5bd67ca3331c190
Jakub Czyz <119632961+maggyy666@users.noreply.github.com>
2025-03-12 12:19:16 +0100

    connect w/ docker + regex

---
+++ api/tenants.py
@@ -0,0 +1,27 @@
+import re
+from opensearchpy import OpenSearch
+
+client = OpenSearch(
+    host = [{'host': 'localhost', 'port': 9200}],
+    http_auth = ('admin', 'StrongPassw0rd!'),
+    use_ssl = True,
+    verify_certs = False
+)
+async def get_tenants():
+    index_name = "tenants"
+    query = {"query": {"match_all": {}}}
+
+    response = client.search(index=index_name, body=query)
+    tenants = []
+    for hit in response['hits']['hits']:
+        description = hit['_source'].get('description','')
+
+        #REGEX
+        match = re.search(r"NAME: (.+), DATA: (.+)", description)
+        name, data = match.groups() if match else ("Unknown", "Unknown")
+        tenants.append({
+            "id": hit['_id'],
+            "name": name,
+            "data": data
+        })
+    return tenants
