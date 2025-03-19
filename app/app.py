e32d7a82ee04b1b79544eb40e06d00f614ed9003
Jakub Czyz <119632961+maggyy666@users.noreply.github.com>
2025-03-19 16:13:21 +0100

    Update app with corrected routes

--- app/app.py
+++ app/app.py
@@ -4,21 +4,15 @@ from api import anomaly_detection, tenants
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
 
+app = connexion.App(__name__, specification_dir='../schema/')
 
-app = connexion.App(__name__, specification_dir='.')
+app.add_api(
+    'openapi.yaml',
+    resolver=RestyResolver('api'),
+    pythonic_params=True,
+    validate_responses=True,
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-    middleware_position=MiddlewarePosition.BEFORE_REQUEST
 )
-
-
-app.add_api('openapi.yaml', resolver=RestyResolver('api'), pythonic_params=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
