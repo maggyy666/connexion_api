97fc9ac169358974ef7426ef290740f206b09990
Jakub Czyz <119632961+maggyy666@users.noreply.github.com>
2025-03-27 19:10:06 +0100

    Update anomaly_detection.py

--- api/anomaly_detection.py
+++ api/anomaly_detection.py
@@ -17,14 +17,25 @@ async def get(tenant_id: str):
         }
     }
     response = client.search(index=index_name, body=query)
+    try:
+            if 'hits' in response and isinstance(response['hits'], dict):
+                if 'total' in response['hits'] and isinstance(response['hits']['total'], dict):
+                    if 'value' in response['hits']['total'] and isinstance(response['hits']['total']['value'], int):
+                        if response['hits']['total']['value'] > 0:
+                            hit = response['hits']['hits'][0]
+                            return {
+                                "detector_id": hit['_source'].get('detector_id','Unknown'),
+                                "detector_name": hit['_source'].get('name','Unknown'),
+                                "detector_description": hit['_source'].get('description','No description available')
+                            }
+                        else:
+                            return {"message": "No data found"}, 404
+                    else:
+                        return {"message": "Invalid value field"}, 404
+                else:
+                     return {"message": "Missing total field"}, 404
+            else:
+                 return {"message": "Missing hits field"}, 404
+    except Exception as e:
+         return {"message": str(e)}, 500
 
-    if response["hits"]["total"]["value"] > 0:
-        hit = response["hits"]["hits"][0]
-        return {
-            "detector_id": hit["_source"].get("detector_id",'Unknown'),
-            "detector_name":hit["_source"].get("name",'Unknown'),
-            "detector_description":hit["_source"].get("description",'No description available')
-
-        }
-    else:
-        return {"message": "Anomaly detection not found"}, 404
