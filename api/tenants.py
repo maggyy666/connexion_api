22a76a9d5dc85f3ed5961461ed0ae30fb293d8bb
Jakub Czyz <119632961+maggyy666@users.noreply.github.com>
2025-03-27 19:25:31 +0100

    Update tenants.py

--- api/tenants.py
+++ api/tenants.py
@@ -14,15 +14,28 @@ async def search():
 
     response = client.search(index=index_name, body=query)
     tenants = []
-    for hit in response['hits']['hits']:
-        description = hit['_source'].get('description','')
+    try:
+        if 'hits' in response and isinstance(response['hits'], dict):
+            if 'hits' in response['hits'] and isinstance(response['hits']['hits'],list):
+                    for hit in response['hits']['hits']:
+                        description = hit['_source'].get('description','')
+                        #REGEX
+                        
+                        match = re.search(r"NAME: (.+), DATA: (.+)", description) if description else None
+                        name, data = match.groups() if match else ("Unknown", "Unknown")
+                        
+                        tenants.append({
+                        "id": hit['_id'],
+                        "name": name,
+                        "data": data
+                        })
 
-        #REGEX
-        match = re.search(r"NAME: (.+), DATA: (.+)", description)
-        name, data = match.groups() if match else ("Unknown", "Unknown")
-        tenants.append({
-            "id": hit['_id'],
-            "name": name,
-            "data": data
-        })
-    return tenants
+                        return tenants
+            else:
+                return {"message": "Invalid hits field"}, 404
+        else:
+            return {"message": "Invalid response"}, 404
+    except Exception as e:
+        return {"message": str(e)}, 500
+    
+    
